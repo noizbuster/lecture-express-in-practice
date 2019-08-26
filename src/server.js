8f1070363dcebba5663c9f5ceec9edc70a419ee5
noizbuster <noizbuster@noizbuster.com>
2019-08-27 00:34:56 +0900

    add middlewares

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const port = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// to allow CORS
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
